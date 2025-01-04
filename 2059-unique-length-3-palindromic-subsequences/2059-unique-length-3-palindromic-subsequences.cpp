4e4d3787e02bcdb182d30e7997c03631da3d077b
MAHESH REDDY <40000936+nagamaheshreddy@users.noreply.github.com>
2025-01-04 16:30:26 +0530

    Time: 171 ms (65.65%), Space: 15.7 MB (82.21%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-        unordered_set<char>seen;
-        unordered_set<char>unique;
-        unordered_map<char,int>mp;
-        int count=0,n=s.size();
+        unordered_set<char> seen;
+        unordered_set<char> unique;
+        unordered_map<char, int> mp;
+        int count = 0, n = s.size();
 
-        for(int i=0;i<n;i++){
-            mp[s[i]]=i;
+        for (int i = 0; i < n; i++) {
+            mp[s[i]] = i;
         }
 
-        int lastIndex,j;
+        int lastIndex, j;
 
-        for(int i=0;i<n;i++){
-            if(seen.find(s[i])==seen.end()){
-                lastIndex=mp[s[i]];
-                for(int j=i+1;j<lastIndex;j++){
+        for (int i = 0; i < n; i++) {
+            if (seen.find(s[i]) == seen.end()) {
+                lastIndex = mp[s[i]];
+                for (int j = i + 1; j < lastIndex; j++) {
                     unique.insert(s[j]);
                 }
-                count+=unique.size();
+                count += unique.size();
                 unique.clear();
             }
-            seen.insert(s[i]);            
+            seen.insert(s[i]);
         }
         return count;
     }
