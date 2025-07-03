001fa3130b60d64104885d18f63121726b6ea73e
MAHESH REDDY <40000936+nagamaheshreddy@users.noreply.github.com>
2025-07-03 20:00:30 +0530

    Time: 3 ms (57.81%), Space: 9.6 MB (67.19%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
@@ -3,15 +3,13 @@ public:
     char kthCharacter(int k) {
         string word="a";
         string append="";
-        bool flag=true;
 
-        while(flag){
+        while(word.length()<k){
             for(char &ch : word){
                 append+=(ch+1);
             }
             word+=append;
             append="";
-            if(word.length()>=k)flag=false;
         }
         return word[k-1];
     }
