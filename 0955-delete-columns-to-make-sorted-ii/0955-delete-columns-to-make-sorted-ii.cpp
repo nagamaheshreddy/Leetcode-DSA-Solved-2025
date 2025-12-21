36476eb785859b1d5b9ec095b2ddac0274be82b7
MAHESH REDDY <40000936+nagamaheshreddy@users.noreply.github.com>
2025-12-21 19:51:12 +0530

    Time: 0 ms (100%), Space: 12.6 MB (99.44%) - LeetHub

--- 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.cpp
+++ 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.cpp
@@ -4,7 +4,6 @@ public:
         int n = strs.size();
         int m = strs[0].size();
 
-        // resolved[i] indicates strs[i] < strs[i+1] is finalized
         vector<bool> resolved(n - 1, false);
         int unresolved = n - 1;
         int deletions = 0;
@@ -12,7 +11,6 @@ public:
         for (int col = 0; col < m && unresolved > 0; col++) {
             bool bad = false;
 
-            // Check for lexicographical violation
             for (int i = 0; i < n - 1; i++) {
                 if (!resolved[i] && strs[i][col] > strs[i + 1][col]) {
                     bad = true;
@@ -25,7 +23,6 @@ public:
                 continue;
             }
 
-            // Lock in strictly ordered row pairs
             for (int i = 0; i < n - 1; i++) {
                 if (!resolved[i] && strs[i][col] < strs[i + 1][col]) {
                     resolved[i] = true;
